53a1e2a597efae1b20ee5dd176c576cb4f36c477
maxiflo88 <maxiflo88@users.noreply.github.com>
2021-07-30 12:07:23 +0300

    added revit module script"

--- calculatePoints.py
+++ calculatePoints.py
@@ -1,94 +1,49 @@
 import numpy as np
-import pandas as pd
 import open3d as o3d
 import os
+from tqdm import tqdm
 from sklearn.svm import SVR
-from sklearn.linear_model import LogisticRegression, LinearRegression, BayesianRidge, ARDRegression
-from tqdm import trange, tqdm
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, Normalizer
+from sklearn.preprocessing import StandardScaler
 
 
-def splitInGrid(path: str, model:str= "SVR", gridSize:int = 1000)->None:
-	'''
-	Split all points in smaller grids
-	'''
-	if os.path.exists(f'{model}.txt'):
-		os.remove(f'{model}.txt')
-	data=np.loadtxt(path, delimiter=' ')
-	data=nearestRadiusPoints(data)
-	maxvalues=np.max(data, axis=0)[:3]
-	minvalues=np.min(data, axis=0)[:3]
-	grid_values= maxvalues-minvalues
-	x_grids, y_grids, _=(np.ceil(grid_values/gridSize)).astype(np.int32)
+def splitNearestNeighbours(path:str, points:int = 1000)->None:
+	data=np.loadtxt(path, delimiter=' ').astype(np.int32)
 
-	print(f'x split:{x_grids} y split:{y_grids}')
-	test=[]
-	for x_count in trange(x_grids):
-		for y_count in range(y_grids):
-			range1=minvalues[:2]+[gridSize*x_count, gridSize*y_count]
-			range2=minvalues[:2]+[gridSize*(x_count+1), gridSize*(y_count+1)]
-			grid=data[np.all(data[:,:2]>=range1[:2], axis=1) & np.all(data[:,:2]<=range2[:2], axis=1)]
-			if grid.size>0:
-				calcBound=calculateBoundary(grid)
-				calcBound.run(model)
-				calcBound.getGrid()
-
-def splitNearestNeighbours(path:str, model:str= "BayesianRidge", points:int = 1000)->None:
-	if os.path.exists(f'{model}.txt'):
-		os.remove(f'{model}.txt')
-	data=np.loadtxt(path, delimiter=' ')
-	data=nearestRadiusPoints(data)
-	nearestPointsCount(data, points, model)
-class calculateBoundary:
-
-	def __init__(self, grid:np.array)->None:
-		self._grid_bounds=grid[grid[:,3]==1]
-		self._grid_inside=grid[grid[:,3]==0]
-		
-
-	def run(self, name:str="BayesianRidge")->None:
-		self.name=name
-		if self._grid_bounds.size>0 and self._grid_inside.size>0:
-			# print(f'bound:{self._grid_bounds.size} inside:{self._grid_inside.size}')
-			X=self._grid_inside[:,:2]
-			y=self._grid_inside[:,2]
-
-			sc_X= StandardScaler()
-			X=sc_X.fit_transform(X)	
-			model=0
-			if name=="BayesianRidge": model= BayesianRidge(compute_score=True)
-			elif name=="ARD": model= ARDRegression(compute_score=True)
-			elif name=="LinearRegression": model= LinearRegression()
-			elif name=="SVR": model= SVR(kernel='poly', C=100, gamma='auto', degree=3, epsilon=.1, coef0=1)
-
-			model.fit(X, y)
-			z_axis=model.predict(sc_X.transform(self._grid_bounds[:,:2]))
-			self._grid_bounds[:,2]=z_axis
-
-	def getGrid(self)->None:
-		if self._grid_bounds.size>0 and self._grid_inside.size==0:
-			pass
-		else:
-			fullgrid= np.concatenate((self._grid_bounds, self._grid_inside), axis=0)
-			with open(f"{self.name}.txt", "a") as myfile:
-				np.savetxt(myfile, fullgrid[:,:3] ,fmt="%.2f,%.2f,%.2f")
+	newdata=nearestRadiusPoints(data)
+	nearestPointsCount(data, newdata, points)
 
 
-def nearestPointsCount(data:np.array, points:int, name:str='BayesianRidge')->None:
-	bounds=data[data[:,3]==1]
-	inner=data[data[:,3]==0]
+def fixInnerPoints(data):
+	pcd = o3d.geometry.PointCloud()
+	pcd.points = o3d.utility.Vector3dVector(data[:,:3])
+	cl, ind = pcd.remove_statistical_outlier(nb_neighbors=16,
+                                                    std_ratio=0.2)
+	mask = np.ones(len(data), np.bool)
+	mask[ind] = 0
+	# display_inlier_outlier(pcd, ind)
+	return mask
+def reconstruct(data, newdata):
+	data[:,2]=newdata[:,0]
+	stayind=np.where(newdata[:,1]!=1)[0]
+	data=data[stayind]
+	np.savetxt(f"test.txt", data[:,:3] ,fmt="%.2f,%.2f,%.2f")
+
+def nearestPointsCount(data:np.array, newdata:np.array, points:int)->None:
+	boundInd=np.where((newdata[:,1]==0) & (data[:,3]==1))[0]
+	innerInd=np.where((newdata[:,1]==0) & (data[:,3]==0))[0]
+	bounds=data[boundInd]
+	inner=data[innerInd]
 	pcd = o3d.geometry.PointCloud()
 	pcd.points = o3d.utility.Vector3dVector(inner[:,:3])
 	pcd.paint_uniform_color([0.5, 0.5, 0.5])
 	pcd_tree = o3d.geometry.KDTreeFlann(pcd)
 	newbounds=bounds.copy()
-	model=0
-	if name=="BayesianRidge": model= BayesianRidge(compute_score=True)
-	elif name=="ARD": model= ARDRegression(compute_score=True)
-	elif name=="LinearRegression": model= LinearRegression()
-	elif name=="SVR": model= SVR(kernel='poly', C=100, gamma='auto', degree=3, epsilon=.1, coef0=1)
+	# if name=="BayesianRidge": model= BayesianRidge(compute_score=True)
+	# elif name=="ARD": model= ARDRegression(compute_score=True)
+	# elif name=="LinearRegression": model= LinearRegression()
+	model= SVR(kernel='poly', C=100, gamma='auto', degree=3, epsilon=.1, coef0=1)
 
-	for count, bound in enumerate(tqdm(bounds)):
+	for count, bound in enumerate(bounds):
 		[k, idx, _] =pcd_tree.search_knn_vector_3d(bound[:3,np.newaxis], points)
 		nearPoints=inner[idx[1:]]
 
@@ -101,10 +56,15 @@ def nearestPointsCount(data:np.array, points:int, name:str='BayesianRidge')->Non
 
 		model.fit(X, y)
 		z_axis=model.predict(sc_X.transform([bound[:2]]))
-		newbounds[count, 2]=z_axis
+		
+		changeBoundEvaluationInd=boundInd[count]
+		newdata[changeBoundEvaluationInd]=[z_axis, 3]
 
-	fullgrid= np.concatenate((newbounds, inner), axis=0)
-	np.savetxt(f"{name}.txt", fullgrid[:,:3] ,fmt="%.2f,%.2f,%.2f")
+	removeInd=fixInnerPoints(inner)
+	innerRemoveInd=innerInd[removeInd]
+	newdata[innerRemoveInd,1]=1
+	reconstruct(data, newdata)
+	np.savetxt(f"result.txt", newdata ,fmt="%.2f,%i")
 
 def nearestRadiusPoints(data:np.array)->np.array:
 	bounds=np.where(data[:,3]==1)[0]
@@ -114,79 +74,30 @@ def nearestRadiusPoints(data:np.array)->np.array:
 
 	pcd_tree = o3d.geometry.KDTreeFlann(pcd)
 	for bound in bounds:		
-		[k, idx, _] = pcd_tree.search_radius_vector_3d(pcd.points[bound], 200)
+		[k, idx, _] = pcd_tree.search_radius_vector_3d(pcd.points[bound], 100)
 		#paint radius 10 blue
 		np.asarray(pcd.colors)[idx[1:], :] = [0, 0, 1]
 		
 	for bound in bounds:
 		#paint center point red
 		pcd.colors[bound] = [1, 0, 0]
-	# o3d.visualization.draw_geometries([pcd])
 
 	color=np.asarray(pcd.colors)
 	remove_indices=np.where(color[:,2]==1)[0]
-	data = np.delete(data, remove_indices, axis=0)
-	np.savetxt('test.txt', data, fmt="%.4f,%.4f,%.4f, %i")
-	return data
-
-def removeCluster(path, min_limit=100, gridSize:int = 1000):
-	data=np.loadtxt(path, delimiter=' ')
-
-	maxvalues=np.max(data, axis=0)[:3]
-	minvalues=np.min(data, axis=0)[:3]
-	grid_values= maxvalues-minvalues
-	x_grids, y_grids, _=(np.ceil(grid_values/gridSize)).astype(np.int32)
-
-	print(f'x split:{x_grids} y split:{y_grids}')
-	test=[]
-	fullgrid=[]
-	for x_count in trange(x_grids):
-		for y_count in range(y_grids):
-			range1=minvalues[:2]+[gridSize*x_count, gridSize*y_count]
-			range2=minvalues[:2]+[gridSize*(x_count+1), gridSize*(y_count+1)]
-			grid=data[np.all(data[:,:2]>=range1[:2], axis=1) & np.all(data[:,:2]<=range2[:2], axis=1)]
-			
-			if grid.size>0:
-	# 			newgrid=np.zeros((grid.shape[0], 6))
-	# 			newgrid[:,:3]=grid[:,:3]
-	# 			newgrid[:,3:]=np.random.randint(1, 255, 3)/255
-	# 			fullgrid.extend(newgrid)
-	# fullgrid=np.asarray(fullgrid)
-	# pcd = o3d.geometry.PointCloud()
-	# pcd.points = o3d.utility.Vector3dVector(fullgrid[:,:3])
-	# pcd.colors=o3d.utility.Vector3dVector(fullgrid[:,3:])
-	# o3d.visualization.draw_geometries([pcd])
-				newgrid=np.zeros((grid.shape[0], 6))
-				newgrid[:,:3]=grid[:,:3]
-				newgrid[:,3:]=np.random.randint(1, 255, 3)/255
-				grid_mean=grid[:,:3].mean(axis=0)
-				mean_diff=grid[:,:3]-grid_mean
-				mean_diff=np.abs(mean_diff)
-				limit_indices=np.where(mean_diff[:,2]>=min_limit)[0]
-				for limit_index in limit_indices:
-					newgrid[limit_index, 3:]=[1, 0, 0]
-				fullgrid.extend(newgrid)
-	fullgrid=np.asarray(fullgrid)
-	pcd = o3d.geometry.PointCloud()
-	pcd.points = o3d.utility.Vector3dVector(fullgrid[:,:3])
-	pcd.colors=o3d.utility.Vector3dVector(fullgrid[:,3:])
-	o3d.visualization.draw_geometries([pcd])
-
-
-	# data_mean=data[:,:3].mean(axis=0)
-	# mean_diff=data[:,:3]-data_mean
-	# mean_diff=np.abs(mean_diff)
-	# limit_indices=np.where(mean_diff[:,2]>=min_limit)[0]
-	# pcd = o3d.geometry.PointCloud()
-	# pcd.points = o3d.utility.Vector3dVector(data[:,:3])
-	# pcd.paint_uniform_color([0.5, 0.5, 0.5])
-	# for limit_index in limit_indices:
-	# 	pcd.colors[limit_index]=[1, 0, 0]
-	# o3d.visualization.draw_geometries([pcd])
+	newdata=np.zeros((data.shape[0],2))
+	newdata[:,0]=data[:,2]
+	newdata[remove_indices,1]=1
+	# data = np.delete(data, remove_indices, axis=0)
+	# np.savetxt('test.txt', data, fmt="%.2f,%.2f,%.2f, %i")
+	return newdata
 
 if __name__ == '__main__':
 	#BayesianRidge, ARD, LinearRegression, SVR
-	path= "../topo.txt"
-	removeCluster(path, 250, 2000)
-	# splitInGrid(path)
-	# splitNearestNeighbours(path, 'SVR', 300)
+	print(os.getcwd())
+	path="./topo.txt"
+	if os.path.exists(path):
+		splitNearestNeighbours(path, 300)
+		print("Completed")
+	else:
+		print(f"Cant find file {path}")
+
---
+++ fixTopo.cs
@@ -0,0 +1,125 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: edgars
+ * Date: 7/30/2021
+ * Time: 11:52
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using Autodesk.Revit.DB.Architecture;
+
+namespace fixTopo
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.DB.Macros.AddInId("C18E33E9-591D-4AB4-ABC8-C980AE758BB5")]
+	public partial class ThisApplication
+	{
+		private void Module_Startup(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Module_Shutdown(object sender, EventArgs e)
+		{
+
+		}
+				const double _feet_to_mm = 25.4 * 12;
+		
+		static int ConvertFeetToMillimetres( double d )
+		{
+		  return (int) ( _feet_to_mm * d + 0.5 );
+		}
+
+	    class MyFailureProcessor : IFailuresPreprocessor
+	    {
+	        public FailureProcessingResult PreprocessFailures(FailuresAccessor failuresAccessor)
+	        {
+	            // TODO: implement code
+	            return FailureProcessingResult.Continue;
+	        }
+	    }
+		private void createTopo(string filename, TopographySurface surface, Document doc){
+			IList<XYZ>points = surface.GetPoints();
+			int count=0;
+			//start editscope
+	        TopographyEditScope scope = new TopographyEditScope(doc, "change topo");
+	        if(scope.IsActive){
+	        	scope.Cancel();
+	        }
+			scope.Start(surface.Id);
+	        using (Transaction t = new Transaction(doc, "create topo"))
+        	{
+		        t.Start();
+		        foreach(string line in File.ReadAllLines(filename)){
+					string[] info=line.Split(',');
+					int changes=Int32.Parse(info[1]);
+					double elevation=UnitUtils.ConvertToInternalUnits(double.Parse(info[0]), DisplayUnitType.DUT_MILLIMETERS);
+					if(changes==3){
+//						surface.MovePoint(points[count], new XYZ(points[count].X, points[count].Y, elevation));
+//						surface.MovePoints(new List<XYZ>(){points[count]}, new XYZ(0, 0, elevation-points[count].Z));
+						surface.ChangePointElevation(points[count], elevation);
+					}
+					else if(changes==1){
+						surface.DeletePoints(new List<XYZ>(){points[count]});
+					}
+					count++;
+				}
+		        t.Commit();
+	        }
+	        MyFailureProcessor failProc = new MyFailureProcessor();
+		    scope.Commit(failProc);
+		}
+		private void runPythonScript(string dir)
+		{
+			
+			var process = new Process();
+			process.StartInfo.FileName=Path.Combine(dir, "calculatePoints.exe");
+			process.StartInfo.WorkingDirectory=dir;
+			
+			process.Start();
+			process.WaitForExit();
+			
+		}
+		private void saveTopoTXT(TopographySurface surface, string filename){
+			List<string>text=new List<string>();
+			foreach(XYZ point in surface.GetPoints()){
+				if(surface.IsBoundaryPoint(point)){
+					text.Add(string.Format("{0} {1} {2} 1", ConvertFeetToMillimetres( point.X ), 
+									        	                       		ConvertFeetToMillimetres( point.Y ), 
+									        	                       		ConvertFeetToMillimetres( point.Z )));
+				}
+				else{
+					text.Add(string.Format("{0} {1} {2} 0", ConvertFeetToMillimetres( point.X ), 
+									        	                       		ConvertFeetToMillimetres( point.Y ), 
+									        	                       		ConvertFeetToMillimetres( point.Z )));
+				}
+			}
+			if(text.Count!=0) File.WriteAllLines(filename, text);
+		}
+		#region Revit Macros generated code
+		private void InternalStartup()
+		{
+			this.Startup += new System.EventHandler(Module_Startup);
+			this.Shutdown += new System.EventHandler(Module_Shutdown);
+		}
+		#endregion
+		public void topo_main()
+		{
+			string dir=@"G:\revit projects\topoProject\topoFixer\output";
+			Document doc =ActiveUIDocument.Document;
+			ElementId ele= ActiveUIDocument.Selection.GetElementIds().FirstOrDefault();
+			TopographySurface surface = doc.GetElement(ele) as TopographySurface;
+			saveTopoTXT(surface, Path.Combine(dir, "topo.txt"));
+			runPythonScript(dir);
+			createTopo(Path.Combine(dir, "result.txt"), surface, doc);
+		}
+	}
+}
